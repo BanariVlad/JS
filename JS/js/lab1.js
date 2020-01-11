8aa311885d777d166fcc8dd8062d37780bbb97e2
BanariVlad <55697871+BanariVlad@users.noreply.github.com>
2020-01-11 11:41:50 +0200

    Add files via upload

--- JS/js/lab1.js
+++ JS/js/lab1.js
@@ -2,7 +2,14 @@ let marks = new Array();
 
 document.getElementById('solve').onclick = function() {
   addElements(Number(document.getElementById('students').value), Number(document.getElementById('mark').value));
-  solutions();
+  //Results
+  let results = findMinMax();
+  document.getElementById('marks').value = marks;
+  document.getElementById('max').value = results[0];
+  document.getElementById('min').value = results[1];
+  document.getElementById('gap').value = results[2];
+  document.getElementById('med').value = results[3];
+  document.getElementById('quality').value = results[4];
 }
 
 const addElements = (students, mark) => {
@@ -31,14 +38,4 @@ const findMinMax = () => {
   });
   let gap = max - min;
   return [max, min, gap, Math.round(sum / marks.length), Math.round(quality * 100 / marks.length)];
-}
-
-const solutions = () => {
-  let results = findMinMax();
-  document.getElementById('marks').value = marks;
-  document.getElementById('max').value = results[0];
-  document.getElementById('min').value = results[1];
-  document.getElementById('gap').value = results[2];
-  document.getElementById('med').value = results[3];
-  document.getElementById('quality').value = results[4];
 }
